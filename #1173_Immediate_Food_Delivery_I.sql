0d868096051b9a95aaaab00e3680386990f1f879
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-04-25 12:20:22 -0700

    Create #1173_Immediate_Food_Delivery_I.sql

---
+++ #1173_Immediate_Food_Delivery_I.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ROUND((SELECT
+        COUNT(*)
+     FROM
+        Delivery
+     WHERE
+        order_date = customer_pref_delivery_date
+    )/COUNT(*) * 100, 2) AS immediate_percentage
+FROM
+    Delivery
