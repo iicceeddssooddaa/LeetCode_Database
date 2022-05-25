8fc5c44868015237be968d6010627f241de4fd27
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-05-25 12:00:57 -0700

    Update #0613_Shortest_Distance_in_a_Line.sql

--- #0613_Shortest_Distance_in_a_Line.sql
+++ #0613_Shortest_Distance_in_a_Line.sql
@@ -1,25 +1,10 @@
-WITH temp AS (
-    SELECT
-        a.x AS num1, b.x AS num2
-    FROM
-        Point AS a
-    JOIN Point AS b
-),
-diff AS (
-    SELECT 
-        ABS(num1 - num2) AS diff
-    FROM
-        temp
-)
-SELECT MIN(diff) AS shortest
-FROM diff
-WHERE diff != 0
-------
 WITH t1 AS (
-    SELECT 
-        x - LAG(x) OVER (ORDER BY x) AS diff
-    FROM
-        Point
+    SELECT x, ROW_NUMBER () OVER () AS code
+    FROM Point
+),
+t2 AS (
+    SELECT ABS(p1.x - p2.x) AS dist
+    FROM t1 AS p1, t1 AS p2
+    WHERE p1.code != p2.code
 )
-SELECT MIN(diff) AS shortest
-FROM t1
+SELECT MIN(dist) AS shortest FROM t2
