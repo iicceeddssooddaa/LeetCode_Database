6b2b73298c28be5a2a02ae33c5df71d32b4ac96f
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-05-27 00:56:28 -0700

    Create #1364_Number_of_Trusted_Contacts_of_a_Customer.sql

---
+++ #1364_Number_of_Trusted_Contacts_of_a_Customer.sql
@@ -0,0 +1,18 @@
+WITH t1 AS (
+    SELECT DISTINCT user_id, COUNT(*) OVER (PARTITION BY user_id) AS contacts_cnt
+    FROM Contacts
+),
+t2 AS (
+    SELECT DISTINCT user_id, COUNT(*) OVER (PARTITION BY user_id) AS trusted_contacts_cnt
+    FROM Contacts
+    WHERE contact_name IN (SELECT customer_name FROM Customers)
+)
+SELECT 
+    invoice_id, customer_name, price, 
+    COALESCE(contacts_cnt,0) AS contacts_cnt, 
+    COALESCE(trusted_contacts_cnt,0) AS trusted_contacts_cnt
+FROM Invoices
+LEFT OUTER JOIN Customers ON Invoices.user_id = Customers.customer_id
+LEFT OUTER JOIN t1 ON Invoices.user_id = t1.user_id
+LEFT OUTER JOIN t2 ON INvoices.user_id = t2.user_id
+ORDER BY invoice_id;
