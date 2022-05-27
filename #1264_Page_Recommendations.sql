be063e80419f46315fac636ae99ccb3ae045f7aa
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-05-26 23:18:12 -0700

    Create #1264_Page_Recommendations.sql

---
+++ #1264_Page_Recommendations.sql
@@ -0,0 +1,11 @@
+WITH t1 AS (
+    SELECT user1_id AS user_id FROM Friendship WHERE user2_id = 1
+    UNION
+    SELECT user2_id AS user_id FROM Friendship WHERE user1_id = 1
+),
+t2 AS (
+    SELECT page_id FROM Likes WHERE user_id = 1
+)
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE user_id IN (SELECT * FROM t1) AND (page_id NOT IN (SELECT * FROM t2));
