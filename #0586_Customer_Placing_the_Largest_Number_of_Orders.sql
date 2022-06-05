f43789b49a80061fdaed00610906ab5b4de90fe0
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-06-05 14:18:15 -0700

    Update #0586_Customer_Placing_the_Largest_Number_of_Orders.sql

--- #0586_Customer_Placing_the_Largest_Number_of_Orders.sql
+++ #0586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -11,3 +11,14 @@ WITH t AS
 SELECT customer_number
 FROM t
 LIMIT 1;
+----------
+WITH t AS (
+    SELECT 
+        DISTINCT customer_number, 
+        COUNT(order_number) OVER (PARTITION BY customer_number) AS order_number
+    FROM Orders
+    ORDER BY order_number DESC
+)
+SELECT customer_number
+FROM t
+LIMIT 1;
