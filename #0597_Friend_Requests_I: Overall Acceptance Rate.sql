0a10cc06604b395f8a70512c50684d6e64015136
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-06-05 14:37:51 -0700

    Update #0597_Friend_Requests_I: Overall Acceptance Rate.sql

--- #0597_Friend_Requests_I: Overall Acceptance Rate.sql
+++ #0597_Friend_Requests_I: Overall Acceptance Rate.sql
@@ -8,3 +8,5 @@ t2 AS ( SELECT COUNT(*) AS accept FROM ac )
 SELECT 
     ROUND( COALESCE ( (SELECT MAX(accept) FROM t2)/ MAX(request), 0), 2) AS accept_rate
 FROM t1
+--------------------
+SELECT ROUND(COALESCE((SELECT COUNT(DISTINCT requester_id, accepter_id) FROM RequestAccepted) / (SELECT COUNT(DISTINCT sender_id, send_to_id) FROM FriendRequest),0),2) AS accept_rate;
