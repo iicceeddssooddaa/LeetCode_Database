f54feb8699b55d8b7a8542e30449d0559ad3ce9a
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-06-05 14:22:55 -0700

    Update #0596_Classes_More_Than_5_Students.sql

--- #0596_Classes_More_Than_5_Students.sql
+++ #0596_Classes_More_Than_5_Students.sql
@@ -2,3 +2,11 @@ SELECT class
 FROM Courses
 GROUP BY class
 HAVING COUNT(class) >=5;
+-------------
+WITH t AS (
+    SELECT DISTINCT class, COUNT(student) OVER (PARTITION BY class) AS cnt
+    FROM Courses
+)
+SELECT class
+FROM t
+WHERE cnt >= 5;
