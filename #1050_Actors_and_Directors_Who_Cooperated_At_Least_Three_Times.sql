4350285fed17a23aea6ee5f31715b617f5b6f92c
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-04 15:33:49 -0700

    Update #1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql

--- #1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
+++ #1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -1,12 +1,4 @@
-WITH t AS (
-    SELECT
-        actor_id, director_id, COUNT(timestamp) AS cnt
-    FROM
-        ActorDirector
-    GROUP BY actor_id, director_id
-)
-SELECT
-    actor_id, director_id
-FROM
-    t
-WHERE cnt >= 3
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
