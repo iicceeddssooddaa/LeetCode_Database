b23145e760b43db85f40cd2fb4683813ea662aa4
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 02:57:26 -0700

    Update #1435_Create_a_Session_Bar_Chart.sql

--- #1435_Create_a_Session_Bar_Chart.sql
+++ #1435_Create_a_Session_Bar_Chart.sql
@@ -1,39 +1,15 @@
-WITH bin1 AS (
-    SELECT
-        "[0-5>" AS bin, COUNT(duration) AS total
-    FROM
-        Sessions
-    WHERE
-        duration < 300
-),
-bin2 AS (
-    SELECT
-        "[5-10>" AS bin, COUNT(duration) AS total
-    FROM
-        Sessions
-    WHERE
-        duration BETWEEN 300 AND 599
-),
-bin3 AS (
-    SELECT
-        "[10-15>" AS bin, COUNT(duration) AS total
-    FROM
-        Sessions
-    WHERE
-        duration BETWEEN 600 AND 899
-),
-bin4 AS (
-    SELECT
-        "15 or more" AS bin, COUNT(duration) AS total
-    FROM
-        Sessions
-    WHERE
-        duration >= 900
-)
-SELECT * FROM bin1
+(SELECT '[0-5>' AS bin, COALESCE(COUNT(1),0) AS total
+FROM Sessions
+WHERE duration BETWEEN 0 AND 299)
 UNION
-SELECT * FROM bin2
+(SELECT '[5-10>' AS bin, COALESCE(COUNT(1),0) AS total
+FROM Sessions
+WHERE duration BETWEEN 300 AND 599)
 UNION
-SELECT * FROM bin3
+(SELECT '[10-15>' AS bin, COALESCE(COUNT(1),0) AS total
+FROM Sessions
+WHERE duration BETWEEN 600 AND 899)
 UNION
-SELECT * FROM bin4
+(SELECT '15 or more' AS bin, COALESCE(COUNT(1),0) AS total
+FROM Sessions
+WHERE duration >=900)
