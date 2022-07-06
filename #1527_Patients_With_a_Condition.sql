348a2969380b5a3d5417455a22d014fc96cefb64
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 03:47:44 -0700

    Update #1527_Patients_With_a_Condition.sql

--- #1527_Patients_With_a_Condition.sql
+++ #1527_Patients_With_a_Condition.sql
@@ -4,3 +4,6 @@ FROM
     Patients
 WHERE
     conditions LIKE "% DIAB1%" OR conditions LIKE "DIAB1%";
+-----------
+SELECT * FROM Patients
+WHERE (conditions REGEXP '^DIAB1') OR (conditions REGEXP ' DIAB1');
