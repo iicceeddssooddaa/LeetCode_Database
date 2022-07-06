66abd7d4d4dce2e95ad8e7ac7297a39f189e1782
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 14:04:52 -0700

    Update #1607_Sellers_With_No_Sales.sql

--- #1607_Sellers_With_No_Sales.sql
+++ #1607_Sellers_With_No_Sales.sql
@@ -1,20 +1,4 @@
-WITH o AS (
-    SELECT 
-        COUNT(sale_date) AS sale_count,
-        seller_id
-    FROM
-        Orders
-    WHERE 
-        sale_date BETWEEN "2020-01-01" AND "2020-12-31"
-    GROUP BY 
-        seller_id
-)
-SELECT 
-    seller_name
-FROM 
-    Seller
-LEFT OUTER JOIN o ON Seller.seller_id = o.seller_id
-WHERE 
-    (sale_count IS NULL) OR
-    (sale_count = 0)
-ORDER By seller_name
+SELECT seller_name
+FROM Seller
+WHERE seller_id NOT IN (SELECT DISTINCT seller_id FROM ORDERS WHERE sale_date BETWEEN '2020-01-01' AND '2020-12-31')
+ORDER BY seller_name;
