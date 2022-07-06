80f0ee2a1c0036cfba7335d6598c88bf707d4d02
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 14:22:33 -0700

    Update #1633_Percentage_of_Users_Attended_a_Contest.sql

--- #1633_Percentage_of_Users_Attended_a_Contest.sql
+++ #1633_Percentage_of_Users_Attended_a_Contest.sql
@@ -1,15 +1,4 @@
-WITH reg AS (
-    SELECT
-        contest_id,
-        COUNT(user_id) AS part
-    FROM Register
-    GROUP BY contest_id
-)
-SELECT
-    contest_id,
-    ROUND(part/(SELECT COUNT(user_id) FROM Users) * 100,2) AS percentage
-FROM
-    reg
-ORDER BY
-    percentage DESC,
-    contest_id
+SELECT DISTINCT contest_id, ROUND(COUNT(DISTINCT user_id)/(SELECT COUNT(DISTINCT user_id) FROM Users)*100,2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
