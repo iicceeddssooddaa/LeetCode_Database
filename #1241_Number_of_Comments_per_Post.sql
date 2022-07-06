23ce4938b738142d5b65f71dc071c6ff9be9af43
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 02:02:28 -0700

    Update #1241_Number_of_Comments_per_Post.sql

--- #1241_Number_of_Comments_per_Post.sql
+++ #1241_Number_of_Comments_per_Post.sql
@@ -1,24 +1,11 @@
-WITH t1 AS ( SELECT DISTINCT sub_id AS post_id FROM Submissions WHERE parent_id IS NULL),
-temp AS (
-    SELECT
-        sub_id, parent_id
-    FROM
-        Submissions
-    WHERE
-        parent_id IN (SELECT * FROM t1)
-    GROUP BY
-        sub_id, parent_id
-    
-),
-t2 AS (
-    SELECT
-        parent_id, COUNT(parent_id) AS number_of_comments
-    FROM
-        temp
-    GROUP BY
-        parent_id
+WITH t AS (
+    SELECT parent_id, COUNT(DISTINCT sub_id) AS number_of_comments 
+    FROM Submissions 
+    WHERE parent_id IS NOT NULL 
+    GROUP BY parent_id
 )
-SELECT post_id, COALESCE(number_of_comments,0) AS number_of_comments
-FROM t1
-LEFT OUTER JOIN t2 ON t1.post_id = t2.parent_id
-ORDER BY post_id;
+SELECT DISTINCT sub_id AS post_id, COALESCE(number_of_comments,0) AS number_of_comments
+FROM Submissions
+LEFT OUTER JOIN t ON Submissions.sub_id = t.parent_id
+WHERE sub_id IN (SELECT sub_id FROM Submissions WHERE parent_id IS NULL)
+ORDER BY sub_id;
