24af0593e10efaf49cb2892425f0559586d762fd
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 02:41:02 -0700

    Update #1350_Students_With_Invalid_Departments.sql

--- #1350_Students_With_Invalid_Departments.sql
+++ #1350_Students_With_Invalid_Departments.sql
@@ -1,13 +1,3 @@
-SELECT
-    id,
-    name
-FROM 
-    Students
-LEFT OUTER JOIN 
-    (SELECT 
-        id AS department_id,
-        name as department_name
-     FROM 
-        Departments) AS Temp
-ON Students.department_id = Temp.department_id
-WHERE department_name IS NULL;
+SELECT id, name
+FROM Students
+WHERE department_id NOT IN (SELECT id FROM Departments);
