0dd9380a8bfb6c17c10dea29ec8d21c26aab2d4f
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 16:18:00 -0700

    Update #1795_Rearrange_Products_Table.sql

--- #1795_Rearrange_Products_Table.sql
+++ #1795_Rearrange_Products_Table.sql
@@ -1,33 +1,5 @@
-WITH s1 AS (
-    SELECT
-        product_id,
-        "store1" AS store,
-        store1 AS price
-    FROM
-        Products
-),
-s2 AS (
-    SELECT
-        product_id,
-        "store2" AS store,
-        store2 AS price
-    FROM
-        Products
-),
-s3 AS (
-    SELECT
-        product_id,
-        "store3" AS store,
-        store3 AS price
-    FROM
-        Products
-),
-temp AS (
-    SELECT * FROM s1
-    UNION
-    SELECT * FROM s2
-    UNION
-    SELECT * FROM s3
-)
-SELECT * FROM temp
-WHERE price IS NOT NULL
+(SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL)
+UNION
+(SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL)
+UNION
+(SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL)
