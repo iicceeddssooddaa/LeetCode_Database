2de1e7299a2c79a2847223602b3b9f11e38d0663
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 14:30:39 -0700

    Update #1661_Average_Time_of_Process_per_Machine.sql

--- #1661_Average_Time_of_Process_per_Machine.sql
+++ #1661_Average_Time_of_Process_per_Machine.sql
@@ -1,34 +1,3 @@
-WITH t1 AS (
-    SELECT
-        machine_id,
-        process_id,
-        timestamp AS start_time
-    FROM
-        Activity
-    WHERE
-        activity_type = "start"
-), 
-t2 AS (
-    SELECT
-        machine_id,
-        process_id,
-        timestamp AS end_time
-    FROM
-        Activity
-    WHERE
-        activity_type = "end"
-),
-temp AS (
-    SELECT t1.*, t2.end_time
-    FROM t1
-    LEFT JOIN t2 ON
-        t1.machine_id = t2.machine_id AND
-        t1.process_id = t2.process_id
-)
-SELECT
-    machine_id,
-    ROUND( (SUM(end_time) - SUM(start_time))/ COUNT(process_id) ,3) AS processing_time
-FROM
-    temp
-GROUP BY
-    machine_id
+SELECT machine_id, ROUND(SUM(IF(activity_type='end',timestamp,-timestamp))/SUM(IF(activity_type='start',1,0)),3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
