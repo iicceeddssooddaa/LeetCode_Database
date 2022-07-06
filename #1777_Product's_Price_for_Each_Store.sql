71110f55d17a5fa83bf3cab787e9afbd8ff5a29d
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 15:59:39 -0700

    Update #1777_Product's_Price_for_Each_Store.sql

--- #1777_Product's_Price_for_Each_Store.sql
+++ #1777_Product's_Price_for_Each_Store.sql
@@ -1,54 +1,5 @@
-WITH s1 AS (
-    SELECT
-        product_id,
-        price AS store1
-    FROM
-        Products
-    WHERE
-        store = "store1"
-),
-s2 AS (
-    SELECT
-        product_id,
-        price AS store2
-    FROM
-        Products
-    WHERE
-        store = "store2"
-),
-s3 AS (
-    SELECT
-        product_id,
-        price AS store3
-    FROM
-        Products
-    WHERE
-        store = "store3"
-),
-coltab AS (
-    SELECT
-        DISTINCT product_id
-    FROM Products
-),
-temp1 AS (
-    SELECT
-        coltab.*, s1.store1
-    FROM
-        coltab
-    LEFT OUTER JOIN s1 ON coltab.product_id = s1.product_id
-),
-temp2 AS (
-    SELECT
-        temp1.*, s2.store2
-    FROM
-        temp1
-    LEFT OUTER JOIN s2 ON temp1.product_id = s2.product_id
-),
-temp3 AS (
-    SELECT
-        temp2.*, s3.store3
-    FROM
-        temp2
-    LEFT OUTER JOIN s3 ON temp2.product_id = s3.product_id
-)
-SELECT * FROM temp3
+SELECT DISTINCT p.product_id, store1, store2, store3
+FROM Products AS p
+LEFT OUTER JOIN (SELECT product_id, price AS store1 FROM Products WHERE store = 'store1') AS p1 ON p.product_id = p1.product_id
+LEFT OUTER JOIN (SELECT product_id, price AS store2 FROM Products WHERE store = 'store2') AS p2 ON p.product_id = p2.product_id
+LEFT OUTER JOIN (SELECT product_id, price AS store3 FROM Products WHERE store = 'store3') AS p3 ON p.product_id = p3.product_id
