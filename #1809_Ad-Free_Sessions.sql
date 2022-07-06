84c676a5569f7d9ea9af7c400ca66c9bf76b5ff1
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 16:29:48 -0700

    Update #1809_Ad-Free_Sessions.sql

--- #1809_Ad-Free_Sessions.sql
+++ #1809_Ad-Free_Sessions.sql
@@ -1,12 +1,4 @@
-WITH temp AS (
-SELECT 
-    Playback.*, Ads.timestamp
-FROM
-    Playback
-LEFT OUTER JOIN Ads ON 
-    Playback.customer_id = Ads.customer_id AND
-    Ads.timestamp BETWEEN Playback.start_time AND Playback.end_time
-)
-SELECT session_id
-FROM temp
-WHERE timestamp IS NULL
+SELECT DISTINCT session_id
+FROM Playback
+LEFT OUTER JOIN Ads ON Playback.customer_id = Ads.customer_id AND (timestamp BETWEEN start_time AND end_time)
+WHERE timestamp IS NULL;
