4cd32fdd54eebe04a54a02e5e282fb51f149d65c
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-06 18:36:18 -0700

    Update #2082_The_Number_of_Rich_Customers.sql

--- #2082_The_Number_of_Rich_Customers.sql
+++ #2082_The_Number_of_Rich_Customers.sql
@@ -1,15 +1,4 @@
-WITH t AS(
-    SELECT 
-        customer_id,
-        MAX(amount) AS large
-    FROM
-        Store
-    GROUP BY
-        customer_id
-)
-
-SELECT
-    COUNT(customer_id) AS rich_count
-FROM
-    t
-WHERE large > 500
+SELECT COUNT(1) AS rich_count FROM (SELECT COUNT(DISTINCT customer_id) AS rich_count
+FROM Store
+GROUP BY customer_id
+HAVING MAX(amount) > 500) AS t;
