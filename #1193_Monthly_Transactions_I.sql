15445539034aeaf06c0a456315e0e8630bc217ba
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 16:20:38 -0700

    Update #1193_Monthly_Transactions_I.sql

--- #1193_Monthly_Transactions_I.sql
+++ #1193_Monthly_Transactions_I.sql
@@ -20,3 +20,14 @@ FROM t1
 LEFT OUTER JOIN t2
 ON t1.month = t2.month AND t1.country = t2.country
 ORDER BY month, country;
+----------
+WITH t AS (
+    SELECT id, country, state, amount, DATE_FORMAT(trans_date, '%Y-%m') AS month
+    FROM Transactions
+)
+SELECT DISTINCT month, country, 
+    COUNT(1) OVER (PARTITION BY month, country) AS trans_count, 
+    SUM(IF(state = 'approved', 1,0)) OVER (PARTITION BY month, country) AS approved_count,
+    SUM(amount) OVER (PARTITION BY month, country) AS trans_total_amount, 
+    SUM(IF(state = 'approved', amount,0)) OVER (PARTITION BY month, country) AS approved_total_amount
+FROM t;
