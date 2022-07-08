31a7250a03d8d522e26d272b7e76f4c4f5c5f177
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 02:45:50 -0700

    Update #0626_Exchange_Seats.sql

--- #0626_Exchange_Seats.sql
+++ #0626_Exchange_Seats.sql
@@ -6,3 +6,11 @@ SELECT id,
     END AS student
 FROM Seat
 ORDER BY id;
+--------
+SELECT id, CASE
+    WHEN NOT (id%2) THEN LAG(student) OVER (ORDER BY id)
+    WHEN (LEAD(id) OVER (ORDER BY id) IS NOT NULL) THEN LEAD (student) OVER (ORDER BY id)
+    ELSE student
+    END AS student
+FROM Seat
+ORDER BY id;
