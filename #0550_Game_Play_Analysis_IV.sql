2d97af85f28360774b25f3424499bd1bfc02bf49
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 00:56:06 -0700

    Update #0550_Game_Play_Analysis_IV.sql

--- #0550_Game_Play_Analysis_IV.sql
+++ #0550_Game_Play_Analysis_IV.sql
@@ -1,7 +1,6 @@
-WITH t1 AS (
-    SELECT player_id, DATEDIFF(event_date, MIN(event_date) OVER (PARTITION BY player_id ORDER BY event_date)) AS date_diff
+WITH t AS (
+    SELECT player_id, DATEDIFF(event_date, MIN(event_date) OVER (PARTITION BY player_id ORDER BY event_date)) = 1 AS flag
     FROM Activity
-    ORDER BY player_id
 )
-SELECT ROUND((SELECT COUNT(DISTINCT player_id) FROM t1 WHERE date_diff = 1)/ COUNT(DISTINCT player_id), 2) AS fraction
-FROM Activity
+SELECT ROUND( (SELECT COUNT(DISTINCT player_id) FROM t WHERE flag)  / ( COUNT(DISTINCT player_id) ),2) AS fraction
+FROM Activity;
