5f9194e6b6cda6a7199448eec65802912a8f510c
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 03:08:46 -0700

    Update #1070_Product_Sales_Analysis_III.sql

--- #1070_Product_Sales_Analysis_III.sql
+++ #1070_Product_Sales_Analysis_III.sql
@@ -6,3 +6,7 @@ SELECT product_id, year AS first_year, quantity, price
 FROM t
 WHERE flag
 ORDER By product_id;
+----------------
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id);
