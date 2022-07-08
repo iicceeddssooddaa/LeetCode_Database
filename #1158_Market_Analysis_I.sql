535fc4412869a267c01b95072bebc3164a4d3be2
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 15:38:54 -0700

    Update #1158_Market_Analysis_I.sql

--- #1158_Market_Analysis_I.sql
+++ #1158_Market_Analysis_I.sql
@@ -7,3 +7,7 @@ SELECT user_id AS buyer_id, join_date, COALESCE(orders_in_2019,0) AS orders_in_2
 FROM Users
 LEFT OUTER JOIN o ON Users.user_id = o.buyer_id
 ORDER BY user_id;
+---------------
+SELECT user_id AS buyer_id, join_date, COALESCE(orders_in_2019,0) AS orders_in_2019
+FROM Users
+LEFT OUTER JOIN (SELECT buyer_id, COUNT(1) AS orders_in_2019 FROM Orders WHERE order_date BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY buyer_id) t ON Users.user_id = t.buyer_id;
