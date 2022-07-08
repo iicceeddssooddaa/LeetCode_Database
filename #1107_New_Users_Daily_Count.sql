0336a9ecc25aeac5d181cc57c175a02ec6b02d8e
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 03:48:05 -0700

    Update #1107_New_Users_Daily_Count.sql

--- #1107_New_Users_Daily_Count.sql
+++ #1107_New_Users_Daily_Count.sql
@@ -1,23 +1,6 @@
-WITH t1 AS (
-    SELECT user_id, activity_date, MIN(activity_date) OVER (PARTITION BY user_id) AS first_login
-    FROM Traffic
-    WHERE activity = 'login'
-),
-t2 AS (
-    SELECT DISTINCT user_id
-    FROM t1
-    WHERE DATEDIFF('2019-06-30',first_login) > 90
-),
-t3 AS (
-    SELECT DISTINCT user_id, activity_date, MIN(activity_date) OVER (PARTITION BY user_id) AS first_login
-    FROM Traffic
-    WHERE activity = 'login' AND user_id NOT IN (SELECT user_id FROM t2)
-),
-t4 AS (
-    SELECT user_id, first_login
-    FROM t3
-    WHERE activity_date = first_login
-)
-SELECT DISTINCT first_login AS login_date, COUNT(user_id) OVER (PARTITION BY first_login) AS user_count
-FROM t4
-ORDER BY login_date;
+SELECT activity_date AS login_date, COUNT(DISTINCT user_id)AS user_count
+FROM Traffic
+WHERE DATEDIFF('2019-06-30',activity_date) <= 90 
+    AND ((user_id, activity_date) IN 
+         (SELECT user_id, MIN(activity_date) FROM Traffic WHERE activity = 'login' GROUP BY user_id ))
+GROUP BY activity_date;
