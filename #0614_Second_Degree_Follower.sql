aa2fd1ea5339fe2ac0c40621cb6e7ac9a4763fef
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 02:28:49 -0700

    Update #0614_Second_Degree_Follower.sql

--- #0614_Second_Degree_Follower.sql
+++ #0614_Second_Degree_Follower.sql
@@ -1,8 +1,4 @@
-WITH t AS (
-    SELECT followee AS person
-    FROM Follow
-    WHERE followee IN (SELECT follower FROM Follow)
-)
-SELECT DISTINCT person AS follower, COUNT(person) OVER (PARTITION BY person) AS num
-FROM t
+SELECT DISTINCT followee AS follower, COUNT(1) OVER (PARTITION BY followee) AS num
+FROM Follow
+WHERE followee IN (SELECT DISTINCT follower FROM Follow)
 ORDER BY follower;
