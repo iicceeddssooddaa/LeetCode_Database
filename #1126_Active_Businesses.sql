8b81b39394518fe46b2968e574c7b5897ee68f7e
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 04:22:00 -0700

    Update #1126_Active_Businesses.sql

--- #1126_Active_Businesses.sql
+++ #1126_Active_Businesses.sql
@@ -11,3 +11,23 @@ t2 AS (
 SELECT business_id
 FROM t2
 WHERE hi_ct > 1
+
+
+
+
+
+
+select 
+business_id
+
+from events as a
+left join
+    (
+    select event_type, avg(occurences) as av
+    from events
+    group by event_type
+    ) as b
+on a.event_type = b.event_type
+where a.occurences > b.av
+group by business_id
+having count(*)>1;
