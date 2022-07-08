d4574ae214a4af27947f97ed4398bebd6bba6ee1
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 00:32:11 -0700

    Update #0178_Rank_Scores.sql

--- #0178_Rank_Scores.sql
+++ #0178_Rank_Scores.sql
@@ -1,8 +1,3 @@
-WITH t AS (
-    SELECT score, ROW_NUMBER() OVER () AS 'rank'
-    FROM (SELECT DISTINCT score FROM Scores ORDER BY score DESC) AS temp
-)
-SELECT Scores.score, t.rank
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
 FROM Scores
-LEFT OUTER JOIN t ON Scores.score = t.score
 ORDER BY score DESC;
