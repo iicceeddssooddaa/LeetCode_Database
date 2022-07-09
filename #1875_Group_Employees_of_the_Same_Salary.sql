bad00179a4211b50723ba8ba8609a618011db49b
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 06:16:44 -0700

    Update #1875_Group_Employees_of_the_Same_Salary.sql

--- #1875_Group_Employees_of_the_Same_Salary.sql
+++ #1875_Group_Employees_of_the_Same_Salary.sql
@@ -1,16 +1,10 @@
-WITH t1 AS (
-    SELECT salary, COUNT(salary) AS ct FROM Employees GROUP BY salary
-    ORDER BY salary
-),
-t2 AS (
-    SELECT salary, ROW_NUMBER() OVER () AS team_id
-    FROM t1
-    WHERE ct != 1
+WITH t AS (
+    SELECT salary, DENSE_RANK() OVER (ORDER BY salary) AS team_id
+    FROM Employees
+    GROUP BY salary
+    HAVING COUNT(salary) > 1
 )
-
-SELECT employee_id, name, Employees.salary, team_id
-FROM Employees
-LEFT OUTER JOIN t2
-ON Employees.salary = t2.salary
-WHERE Employees.salary IN (SELECT salary FROM t2)
+SELECT employee_id, name, t.salary, team_id
+FROM t
+LEFT OUTER JOIN Employees ON t.salary = Employees.salary
 ORDER BY team_id, employee_id;
