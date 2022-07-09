47292720ff3c59d9c22f9c86618afb873ffee2b3
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 19:13:37 -0700

    Update #1270_All_People_Report_to_the_Given_Manager.sql

--- #1270_All_People_Report_to_the_Given_Manager.sql
+++ #1270_All_People_Report_to_the_Given_Manager.sql
@@ -12,3 +12,9 @@ UNION
 SELECT employee_id FROM t2
 UNION
 SELECT employee_id FROM t3
+--------------------
+SELECT e1.employee_id
+FROM Employees AS e1
+INNER JOIN Employees AS e2 ON e1.manager_id = e2.employee_id
+INNER JOIN Employees AS e3 ON e2.manager_id = e3.employee_id
+WHERE (e1.manager_id = 1 OR e2.manager_id = 1 OR e3.manager_id = 1) AND NOT (e1.employee_id = 1);
