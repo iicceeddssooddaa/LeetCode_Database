42a4d14cc47f2200d40c4b2e6991af85146f34f2
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-08 19:05:11 -0700

    Update #1264_Page_Recommendations.sql

--- #1264_Page_Recommendations.sql
+++ #1264_Page_Recommendations.sql
@@ -9,3 +9,8 @@ t2 AS (
 SELECT DISTINCT page_id AS recommended_page
 FROM Likes
 WHERE user_id IN (SELECT * FROM t1) AND (page_id NOT IN (SELECT * FROM t2));
+--------
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE (page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1))
+    AND user_id IN ((SELECT user2_id AS user_id FROM Friendship WHERE user1_id = 1) UNION (SELECT user1_id AS user_id FROM Friendship WHERE user2_id = 1));
