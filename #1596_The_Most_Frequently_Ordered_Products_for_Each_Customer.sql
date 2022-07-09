dc2714c928eb11b88ef6af1e7950eb62545e16b1
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:50:25 -0700

    Update #1596_The_Most_Frequently_Ordered_Products_for_Each_Customer.sql

--- #1596_The_Most_Frequently_Ordered_Products_for_Each_Customer.sql
+++ #1596_The_Most_Frequently_Ordered_Products_for_Each_Customer.sql
@@ -1,17 +1,9 @@
-WITH t1 AS (
-    SELECT 
-        customer_id, product_id, 
-        COUNT(*) OVER(PARTITION BY customer_id, product_id) AS ct
-    FROM
-        Orders
-),
-t2 AS (
-    SELECT customer_id, product_id, ct, MAX(ct) OVER (PARTITION BY customer_id) AS max_ct
-    FROM t1
+WITH t AS (
+    SELECT customer_id, product_id, COUNT(1) AS cnt
+    FROM Orders
+    GROUP BY customer_id, product_id
 )
-SELECT DISTINCT customer_id, t2.product_id, product_name
-FROM t2
-LEFT OUTER JOIN Products
-ON t2.product_id = Products.product_id
-WHERE ct = max_ct
-ORDER BY customer_id, product_id;
+SELECT DISTINCT customer_id, t.product_id, product_name
+FROM t
+LEFT OUTER JOIN Products ON Products.product_id = t.product_id
+WHERE (customer_id, cnt) IN (SELECT customer_id, MAX(cnt) FROM t GROUP BY customer_id);
