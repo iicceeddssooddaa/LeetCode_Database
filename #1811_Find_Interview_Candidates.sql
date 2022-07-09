8e39d80f30502e9ff4fe91876709b3393d0abfe7
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 05:48:23 -0700

    Update #1811_Find_Interview_Candidates.sql

--- #1811_Find_Interview_Candidates.sql
+++ #1811_Find_Interview_Candidates.sql
@@ -1,28 +1,16 @@
 WITH t1 AS (
-    SELECT contest_id, gold_medal AS user_id FROM Contests
-    UNION
-    SELECT contest_id, silver_medal AS user_id FROM Contests
-    UNION
-    SELECT contest_id, bronze_medal AS user_id FROM Contests
+    SELECT DISTINCT user_id, contest_id - LAG(contest_id,2) OVER (PARTITION BY user_id ORDER BY contest_id) = 2 AS flag
+    FROM ((SELECT contest_id, gold_medal AS user_id FROM Contests) 
+    UNION ALL 
+    (SELECT contest_id, silver_medal AS user_id FROM Contests)
+    UNION ALL
+    (SELECT contest_id, bronze_medal AS user_id FROM Contests)) t
 ),
 t2 AS (
-    SELECT DISTINCT gold_medal AS user_id, COUNT(*) OVER (PARTITION BY gold_medal) AS gold_cnt
-    FROM Contests
-),
-t3 AS (
-    SELECT user_id, contest_id - LAG(contest_id,2) OVER (PARTITION BY user_id ORDER BY contest_id) AS flag
-    FROM t1
-),
-t4 AS (
-    (SELECT DISTINCT user_id
-    FROM t3
-    WHERE flag = 2)
-    UNION
-    (SELECT DISTINCT user_id
-    FROM t2
-    WHERE gold_cnt >= 3)
+    (SELECT gold_medal AS user_id FROM Contests GROUP BY gold_medal HAVING COUNT(1) >= 3) 
+    UNION 
+    (SELECT user_id FROM t1 WHERE t1.flag)
 )
 SELECT DISTINCT name, mail
-FROM Users
-INNER JOIN t4 ON Users.user_id = t4.user_id
-ORDER BY name;
+FROM t2
+LEFT OUTER JOIN Users ON t2.user_id = Users.user_id;
