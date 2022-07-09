80522f8a87a55397744cc9feb7f2ffaf451897a0
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 15:13:15 -0700

    Update #2051_The_Category_of_Each_Member_in_the_Store.sql
    COUNT自动略过null，比sum if快很多

--- #2051_The_Category_of_Each_Member_in_the_Store.sql
+++ #2051_The_Category_of_Each_Member_in_the_Store.sql
@@ -1,25 +1,13 @@
-WITH v AS (
-    SELECT DISTINCT member_id, COUNT(*) OVER (PARTITION BY member_id) AS visit_cnt
+WITH t AS (
+    SELECT member_id, 100 * (COUNT(charged_amount))/(COUNT(Visits.visit_id)) AS conv_rate
     FROM Visits
-),
-p AS (
-    SELECT DISTINCT member_id, COUNT(*) OVER (PARTITION BY member_id) AS purchase_cnt
-    FROM Purchases
-    LEFT OUTER JOIN Visits ON Purchases.visit_id = Visits.visit_id
-),
-c AS (
-    SELECT v.member_id, 100 * COALESCE(purchase_cnt,0) / visit_cnt AS conv_rate
-    FROM v
-    LEFT OUTER JOIN p ON v.member_id = p.member_id
+    LEFT OUTER JOIN Purchases ON Visits.visit_id = Purchases.visit_id
+    GROUP BY member_id
 )
-SELECT 
-    Members.member_id, name, 
-    CASE
-        WHEN conv_rate >= 80 THEN 'Diamond'
-        WHEN conv_rate >= 50 AND conv_rate < 80 THEN 'Gold'
-        WHEN conv_rate < 50 THEN 'Silver'
-        ELSE 'Bronze'
-    END AS category
-FROM Members
-LEFT OUTER JOIN c ON Members.member_id = c.member_id
-ORDER BY member_id;
+SELECT m.member_id, name, CASE
+    WHEN conv_rate >=80 THEN 'Diamond'
+    WHEN conv_rate BETWEEN 50 AND 80 THEN 'Gold'
+    WHEN conv_rate <50 THEN 'Silver'
+    ELSE 'Bronze' END AS category
+FROM Members AS m
+LEFT OUTER JOIN t ON m.member_id = t.member_id;
