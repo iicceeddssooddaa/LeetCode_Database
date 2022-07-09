ca4154b247072683314e0fe72a44ac17653cfbbb
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 02:36:00 -0700

    Update #1440_Evaluate_Boolean_Expression.sql

--- #1440_Evaluate_Boolean_Expression.sql
+++ #1440_Evaluate_Boolean_Expression.sql
@@ -1,9 +1,7 @@
-SELECT left_operand, operator, right_operand,
-    IF(CASE
-        WHEN operator = '>' THEN v1.value > v2.value
-        WHEN operator = '<' THEN v1.value < v2.value
-        ELSE v1.value = v2.value
-    END, 'true', 'false') AS value
-FROM Expressions
-LEFT OUTER JOIN `Variables` AS v1 ON Expressions.left_operand = v1.name
-LEFT OUTER JOIN `Variables` AS v2 ON Expressions.right_operand = v2.name
+SELECT left_operand, operator, right_operand, IF(CASE
+    WHEN operator = '>' THEN v1.value > v2.value
+    WHEN operator = '<' THEN v1.value < v2.value
+    ELSE v1.value = v2.value
+    END, 'true', 'false') AS `value`
+FROM Expressions, `Variables` AS v1, `Variables` As v2
+WHERE left_operand = v1.name AND right_operand = v2.name;
