d7c9dc02e8bf32c485999a76ed23bcf57892084d
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 05:52:36 -0700

    Update #1831_Maximum_Transaction_Each_Day.sql

--- #1831_Maximum_Transaction_Each_Day.sql
+++ #1831_Maximum_Transaction_Each_Day.sql
@@ -6,3 +6,13 @@ SELECT transaction_id
 FROM t
 WHERE amount = day_max
 ORDER BY transaction_id
+--------
+WITH t AS (
+    SELECT transaction_id, amount = MAX(amount) OVER w AS flag
+    FROM Transactions
+    WINDOW w AS (PARTITION BY DATE_FORMAT(day, '%Y-%m-%d'))
+)
+SELECT transaction_id
+FROM t
+WHERE flag
+ORDER BY transaction_id;
