73c8cf2c967d99e52107f26a6ccdd8138dec7931
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 14:52:55 -0700

    Update #2041_Accepted_Candidates_From_the_Interviews.sql

--- #2041_Accepted_Candidates_From_the_Interviews.sql
+++ #2041_Accepted_Candidates_From_the_Interviews.sql
@@ -8,3 +8,8 @@ LEFT OUTER JOIN r
 ON Candidates.interview_id = r.interview_id
 WHERE years_of_exp >= 2 AND total_score > 15
 ORDER BY candidate_id;
+---------
+SELECT candidate_id
+FROM Candidates AS c
+INNER JOIN (SELECT DISTINCT interview_id, SUM(score) AS total FROM Rounds GROUP BY interview_id) t ON c.interview_id = t.interview_id
+WHERE years_of_exp >=2 AND total >15;
