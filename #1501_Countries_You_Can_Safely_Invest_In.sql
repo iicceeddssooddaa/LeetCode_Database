20d91ffebd44ca761cedf5806e0f50e879ccc61f
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:17:27 -0700

    Update #1501_Countries_You_Can_Safely_Invest_In.sql

--- #1501_Countries_You_Can_Safely_Invest_In.sql
+++ #1501_Countries_You_Can_Safely_Invest_In.sql
@@ -1,24 +1,14 @@
 WITH t1 AS (
-    SELECT id, Country.name
-    FROM Person
-    LEFT OUTER JOIN Country ON LEFT(Person.phone_number,3) = Country.country_code
+    SELECT caller_id AS id, duration FROM Calls
+    UNION ALL
+    SELECT callee_id AS id, duration FROM Calls
 ),
-t2 AS (
-    SELECT DISTINCT name, SUM(duration) OVER (PARTITION BY name) AS caller_dur, COUNT(*) OVER(PARTITION BY name) AS caller_cnt
-    FROM Calls
-    LEFT OUTER JOIN t1 ON Calls.caller_id = t1.id
-),
-t3 AS (
-    SELECT DISTINCT name, SUM(duration) OVER (PARTITION BY name) AS callee_dur, COUNT(*) OVER(PARTITION BY name) AS callee_cnt
-    FROM Calls
-    LEFT OUTER JOIN t1 ON Calls.callee_id = t1.id
-),
-t4 AS (
-    SELECT Country.name, COALESCE((COALESCE(caller_dur,0) + COALESCE(callee_dur,0)) / (COALESCE(caller_cnt,0) + COALESCE(callee_cnt,0)),0) AS avg_len
-    FROM Country
-    LEFT OUTER JOIN t2 ON Country.name = t2.name
-    LEFT OUTER JOIN t3 ON Country.name = t3.name
+t2 AS(
+    SELECT DISTINCT Country.name, AVG(duration) OVER (PARTITION BY Country.name) AS country_len, AVG(duration) OVER () AS avg_len
+    FROM t1
+    LEFT OUTER JOIN Person ON t1.id = Person.id
+    LEFT OUTER JOIN Country ON LEFT(phone_number,3) = Country.country_code
 )
-SELECT name AS country FROM t4
-WHERE avg_len > (SELECT AVG(duration)FROM Calls)
-ORDER BY name;
+SELECT name AS country
+FROM t2
+WHERE country_len > avg_len;
