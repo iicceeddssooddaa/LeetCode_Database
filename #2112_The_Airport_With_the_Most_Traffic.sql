251649a1586d51fca9c37f85b896edf09a50645f
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 16:08:23 -0700

    Update #2112_The_Airport_With_the_Most_Traffic.sql

--- #2112_The_Airport_With_the_Most_Traffic.sql
+++ #2112_The_Airport_With_the_Most_Traffic.sql
@@ -1,21 +1,10 @@
-WITH t1 AS (
-    (SELECT 
-        DISTINCT departure_airport AS airport_id, 
-        SUM(flights_count) OVER (PARTITION BY departure_airport) AS flights_count 
-    FROM Flights)
+WITH t AS (
+    SELECT id1 AS airport_id, SUM(flights_count) = MAX(SUM(flights_count)) OVER () AS flag
+    FROM ((SELECT departure_airport AS id1, arrival_airport AS id2, flights_count FROM Flights)
     UNION
-    (SELECT 
-        DISTINCT arrival_airport AS airport_id, 
-        SUM(flights_count) OVER (PARTITION BY arrival_airport) AS flights_count 
-    FROM Flights)
-),
-t2 AS (
-    SELECT DISTINCT airport_id, SUM(flights_count) OVER (PARTITION BY airport_id) AS total FROM t1
-),
-t3 AS (
-    SELECT airport_id, total, RANK() OVER (ORDER BY total DESC) AS rank_total FROM t2
+    (SELECT arrival_airport AS id1, departure_airport AS id2, flights_count FROM Flights)) AS f
+    GROUP BY id1
 )
 SELECT airport_id
-FROM t3
-WHERE rank_total = 1
-ORDER BY airport_id;
+FROM t
+WHERE flag;
