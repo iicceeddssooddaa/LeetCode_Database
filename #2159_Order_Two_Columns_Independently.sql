529a0e2f15388eeadfdbb65cce75a76036c8c198
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 16:28:54 -0700

    Update #2159_Order_Two_Columns_Independently.sql

--- #2159_Order_Two_Columns_Independently.sql
+++ #2159_Order_Two_Columns_Independently.sql
@@ -1,12 +1,5 @@
-WITH t1 AS (
-    SELECT first_col, ROW_NUMBER() OVER (ORDER BY first_col) AS fir_rank
-    FROM Data
-),
-t2 AS (
-    SELECT second_col, ROW_NUMBER() OVER (ORDER BY second_col DESC) AS sec_rank
-    FROM Data
-)
 SELECT first_col, second_col
-FROM t1
-LEFT OUTER JOIN t2
-ON t1.fir_rank = t2.sec_rank;
+FROM (SELECT first_col, ROW_NUMBER() OVER (ORDER BY first_col) AS rnk FROM Data) AS t1,
+    (SELECT second_col, ROW_NUMBER() OVER (ORDER BY second_col DESC) AS rnk FROM Data) AS t2
+WHERE t1.rnk = t2.rnk
+ORDER BY t1.rnk;
