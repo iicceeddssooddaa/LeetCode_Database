0bc0cd1119130bb062173c06075f0198fda2665c
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 05:08:31 -0700

    Update #1715_Count_Apples_and_Oranges.sql

--- #1715_Count_Apples_and_Oranges.sql
+++ #1715_Count_Apples_and_Oranges.sql
@@ -1,7 +1,3 @@
-WITH t AS (
-    SELECT box_id, b.chest_id, b.apple_count + COALESCE(c.apple_count,0) AS apple_count, b.orange_count + COALESCE(c.orange_count,0) AS orange_count
-    FROM Boxes AS b
-    LEFT OUTER JOIN Chests AS c
-        ON b.chest_id = c.chest_id
-)
-SELECT (SELECT SUM(apple_count) FROM t) AS apple_count, (SELECT SUM(orange_count) FROM t) AS orange_count
+SELECT SUM(b.apple_count + COALESCE(c.apple_count,0)) AS apple_count, SUM(b.orange_count + COALESCE(c.orange_count,0)) AS orange_count
+FROM Boxes AS b
+LEFT OUTER JOIN Chests AS c ON b.chest_id = c.chest_id;
