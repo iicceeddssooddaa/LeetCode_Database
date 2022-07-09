8e5831977d0a5f5c55071070eb855403ddbc8fdd
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:10:28 -0700

    Update #1468_Calculate_Salaries.sql

--- #1468_Calculate_Salaries.sql
+++ #1468_Calculate_Salaries.sql
@@ -1,13 +1,11 @@
-WITH t1 AS (
-    SELECT 
-        DISTINCT company_id, 
-        CASE
-            WHEN MAX(salary) OVER (PARTITION BY company_id) < 1000 THEN 100
-            WHEN MAX(salary) OVER (PARTITION BY company_id) > 10000 THEN 51
-            ELSE 76
+WITH t AS (
+    SELECT DISTINCT company_id, CASE
+        WHEN MAX(salary) OVER (PARTITION BY company_id) > 10000 THEN .51
+        WHEN MAX(salary) OVER (PARTITION BY company_id) < 1000 THEN 1
+        ELSE .76
         END AS rate
     FROM Salaries
 )
-SELECT Salaries.company_id, employee_id, employee_name, ROUND(salary * rate / 100) AS salary
-FROM Salaries
-LEFT OUTER JOIN t1 ON Salaries.company_id = t1.company_id
+SELECT s.company_id, employee_id, employee_name, ROUND(salary * rate) AS salary
+FROM Salaries AS s
+LEFT OUTER JOIN t ON s.company_id = t.company_id;
