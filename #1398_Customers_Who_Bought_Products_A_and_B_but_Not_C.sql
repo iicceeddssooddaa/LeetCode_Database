0b6d6a4cf6dc40dd6fcde1461ad2a1a23b2a384c
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 02:34:43 -0700

    Update #1398_Customers_Who_Bought_Products_A_and_B_but_Not_C.sql

--- #1398_Customers_Who_Bought_Products_A_and_B_but_Not_C.sql
+++ #1398_Customers_Who_Bought_Products_A_and_B_but_Not_C.sql
@@ -23,3 +23,10 @@ SELECT t.customer_id, customer_name
 FROM t
 LEFT OUTER JOIN Customers
     ON t.customer_id = Customers.customer_id
+---------------
+SELECT c.customer_id, customer_name
+FROM Customers AS c
+INNER JOIN (SELECT DISTINCT customer_id FROM Orders WHERE product_name = 'A') AS a ON c.customer_id = a.customer_id
+INNER JOIN (SELECT DISTINCT customer_id FROM Orders WHERE product_name = 'B') AS b ON c.customer_id = b.customer_id
+WHERE c.customer_id NOT IN (SELECT DISTINCT customer_id FROM Orders WHERE product_name = 'C')
+ORDER BY c.customer_id;
