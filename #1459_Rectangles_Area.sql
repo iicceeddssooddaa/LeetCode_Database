dab3e5e25218ccd6eb25744e63d2c483c157a7f5
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:04:59 -0700

    Update #1459_Rectangles_Area.sql

--- #1459_Rectangles_Area.sql
+++ #1459_Rectangles_Area.sql
@@ -3,3 +3,13 @@ SELECT p1.id AS p1, p2.id AS p2,
 FROM Points AS p1, Points AS p2
 WHERE p1.id < p2.id AND ABS((p1.x_value - p2.x_value) * (p1.y_value - p2.y_value)) != 0
 ORDER BY area DESC, p1, p2;
+----
+WITH t AS (
+    SELECT DISTINCT p1.id AS p1, p2.id AS p2, ABS(p1.x_value - p2.x_value) * ABS(p1.y_value - p2.y_value) AS area
+    FROM Points AS p1, Points AS p2
+    WHERE p1.id < p2.id
+)
+SELECT p1, p2, area
+FROM t
+WHERE area
+ORDER BY area DESC, p1, p2;
