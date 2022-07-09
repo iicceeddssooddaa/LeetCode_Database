ec201ded4f48f627faa625139f7f75c7bfa52cce
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 02:07:30 -0700

    Update #1355_Activity_Participants.sql

--- #1355_Activity_Participants.sql
+++ #1355_Activity_Participants.sql
@@ -11,3 +11,13 @@ LEFT OUTER JOIN Activities
 ON Activities.name = t2.activity
 WHERE freq != max_act AND freq != min_act
 ORDER BY activity;
+--------
+WITH t AS (
+    SELECT DISTINCT Activities.name, COUNT(Friends.name) OVER (PARTITION BY Activities.name) AS cnt
+    FROM Activities
+    LEFT OUTER JOIN Friends ON Friends.activity = Activities.name
+    ORDER BY cnt DESC
+)
+SELECT name AS activity
+FROM t
+WHERE cnt != (SELECT cnt FROM t LIMIT 1) AND cnt != (SELECT cnt FROM t ORDER BY cnt LIMIT 1);
