c55759856b6286e3c51b3221c054febfd4c0ac65
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:38:27 -0700

    Update #1555_Bank_Account_Summary.sql

--- #1555_Bank_Account_Summary.sql
+++ #1555_Bank_Account_Summary.sql
@@ -1,18 +1,10 @@
-WITH t1 AS (
-    SELECT DISTINCT paid_by AS user_id, SUM(-amount) OVER (PARTITION BY paid_by) AS paid
-    FROM Transactions
-),
-t2 AS (
-    SELECT DISTINCT paid_to AS user_id, SUM(amount) OVER (PARTITION BY paid_to) AS received
-    FROM Transactions
+WITH t AS (
+    SELECT paid_by AS user_id, -amount AS amount FROM Transactions
+    UNION ALL
+    SELECT paid_to AS user_id, amount FROM Transactions
 )
-SELECT 
-    Users.user_id, user_name, credit + COALESCE(paid,0) + COALESCE(received,0) AS credit,
-    CASE
-        WHEN credit + COALESCE(paid,0) + COALESCE(received,0) >= 0 THEN 'No'
-        ELSE 'Yes'
-    END AS credit_limit_breached
+SELECT DISTINCT Users.user_id, user_name, credit + COALESCE(SUM(amount) OVER (PARTITION BY Users.user_id),0) AS credit, CASE
+    WHEN credit + COALESCE(SUM(amount) OVER (PARTITION BY Users.user_id),0) >= 0 THEN 'No'
+    ELSE 'Yes' END AS credit_limit_breached
 FROM Users
-LEFT OUTER JOIN t1 ON Users.user_id = t1.user_id
-LEFT OUTER JOIN t2 ON Users.user_id = t2.user_id
-ORDER BY user_id;
+LEFT OUTER JOIN t ON Users.user_id = t.user_id;
