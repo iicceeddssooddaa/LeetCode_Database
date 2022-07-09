85621609faeb2f9aff9aa4744b08080023938584
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 07:29:35 -0700

    Update #1990_Count_the_Number_of_Experiments.sql

--- #1990_Count_the_Number_of_Experiments.sql
+++ #1990_Count_the_Number_of_Experiments.sql
@@ -1,21 +1,22 @@
-WITH t1 AS (
-    SELECT 
-        DISTINCT platform, experiment_name, 
-        COUNT(*) OVER (PARTITION BY platform, experiment_name) AS num_experiments
-    FROM Experiments
-),
-t2 AS (
+WITH t AS (
     SELECT platform, experiment_name
-    FROM 
-        (SELECT 'Android' AS platform 
-         UNION SELECT 'IOS' AS platform 
-         UNION SELECT 'Web' AS platform) AS p, 
-        (SELECT 'Reading' AS experiment_name 
-         UNION SELECT 'Sports' AS experiment_name 
-         UNION SELECT 'Programming' AS experiment_name) AS en
+    FROM ((SELECT 'Android' AS platform) UNION (SELECT 'IOS' AS platform) UNION (SELECT 'Web' AS platform)) AS p, 
+    ((SELECT 'Reading' AS experiment_name) UNION (SELECT 'Sports' AS experiment_name) UNION (SELECT 'Programming' AS experiment_name)) AS e
 )
-SELECT t2.platform, t2.experiment_name, COALESCE(num_experiments,0) AS num_experiments
-FROM t2
-LEFT OUTER JOIN t1
-ON t2.platform = t1.platform AND t2.experiment_name = t1.experiment_name
-ORDER BY platform, experiment_name;
+SELECT t.platform, t.experiment_name, COALESCE(cnt,0) AS num_experiments
+FROM t
+LEFT OUTER JOIN (
+    SELECT platform, experiment_name, COUNT(experiment_id) AS cnt
+    FROM Experiments
+    GROUP BY platform, experiment_name
+) AS e ON t.platform = e.platform AND t.experiment_name = e.experiment_name;
+
+
+
+
+----------------
+select pm platform, name experiment_name, count(experiment_id) num_experiments
+from (values('Android'), ('IOS'), ('Web')) p(pm) cross join
+(values('Reading'), ('Sports'), ('Programming')) e(name)
+left join experiments on p.pm = platform and e.name = experiment_name
+group by pm, name
