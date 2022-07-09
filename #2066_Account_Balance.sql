bfb607a0180f946e17bd706cae9a4b5c80402215
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 15:17:53 -0700

    Update #2066_Account_Balance.sql

--- #2066_Account_Balance.sql
+++ #2066_Account_Balance.sql
@@ -1,13 +1,3 @@
-WITH t AS (
-    SELECT account_id, day, 
-        CASE 
-            WHEN type = 'Withdraw' THEN -amount
-            ELSE amount
-        END AS increment
-    FROM Transactions
-)
-SELECT 
-    account_id, day, 
-    SUM(increment) OVER (PARTITION BY account_id ORDER BY day) AS balance
-FROM t
-ORDER BY account_id, day
+SELECT account_id, day, SUM(IF(type='Deposit',amount,-amount)) OVER(PARTITION BY account_id ORDER By day) AS balance
+FROM Transactions
+ORDER BY account_id, day;
