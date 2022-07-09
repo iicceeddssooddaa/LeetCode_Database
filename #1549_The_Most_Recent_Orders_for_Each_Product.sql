2c7b46fedd54879b2322930c25c1379d34408e85
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 04:32:14 -0700

    Update #1549_The_Most_Recent_Orders_for_Each_Product.sql

--- #1549_The_Most_Recent_Orders_for_Each_Product.sql
+++ #1549_The_Most_Recent_Orders_for_Each_Product.sql
@@ -10,3 +10,9 @@ LEFT OUTER JOIN Products
 ON t.product_id = Products.product_id
 WHERE order_date = latest
 ORDER BY product_name, product_id, order_id;
+----
+SELECT product_name, Orders.product_id, order_id, order_date
+FROM Orders
+LEFT OUTER JOIN Products ON Orders.product_id = Products.product_id
+WHERE (Orders.product_id, order_date) IN (SELECT DISTINCT product_id, MAX(order_date) OVER (PARTITION BY product_id) AS first_day FROM Orders)
+ORDER BY product_name, product_id, order_id;
