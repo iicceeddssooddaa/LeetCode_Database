70bdea071aad7c71994677a7a7e1c9f1a4388271
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 02:23:11 -0700

    Update #1364_Number_of_Trusted_Contacts_of_a_Customer.sql

--- #1364_Number_of_Trusted_Contacts_of_a_Customer.sql
+++ #1364_Number_of_Trusted_Contacts_of_a_Customer.sql
@@ -16,3 +16,13 @@ LEFT OUTER JOIN Customers ON Invoices.user_id = Customers.customer_id
 LEFT OUTER JOIN t1 ON Invoices.user_id = t1.user_id
 LEFT OUTER JOIN t2 ON INvoices.user_id = t2.user_id
 ORDER BY invoice_id;
+-----------
+WITH t AS (
+    SELECT DISTINCT customer_id, customer_name, COUNT(contact_email) OVER (PARTITION BY customer_id) AS contacts_cnt, SUM(IF(contact_email IN (SELECT email FROM Customers),1,0)) OVER (PARTITION BY customer_id) AS trusted_contacts_cnt
+    FROM Customers
+    LEFT OUTER JOIN Contacts ON Customers.customer_id = Contacts.user_id
+)
+SELECT invoice_id, customer_name, price, contacts_cnt, trusted_contacts_cnt
+FROM Invoices
+LEFT OUTER JOIN t ON Invoices.user_id = t.customer_id
+ORDER BY invoice_id;
