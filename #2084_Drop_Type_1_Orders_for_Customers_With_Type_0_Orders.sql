857704667a0748976dda8dad4196e94bc07bf6cb
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 15:32:43 -0700

    Update #2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql

--- #2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql
+++ #2084_Drop_Type_1_Orders_for_Customers_With_Type_0_Orders.sql
@@ -5,3 +5,8 @@ WHERE (customer_id NOT IN (
     FROM Orders
     WHERE order_type = 0
 )) OR (order_type != 1)
+-------
+(SELECT order_id, customer_id, 0 AS order_type FROM Orders WHERE NOT order_type)
+UNION
+(SELECT order_id, customer_id, 1 AS order_type FROM Orders
+WHERE customer_id NOT IN (SELECT customer_id FROM Orders WHERE NOT order_type))
