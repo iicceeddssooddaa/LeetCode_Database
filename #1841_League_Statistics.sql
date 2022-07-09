041d66863f51ebfa93cbbeff28fd743ed270e8ca
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 06:01:45 -0700

    Update #1841_League_Statistics.sql

--- #1841_League_Statistics.sql
+++ #1841_League_Statistics.sql
@@ -1,38 +1,21 @@
-WITH h AS (
-    SELECT 
-        DISTINCT home_team_id AS team_id, 
-        SUM(home_team_goals) OVER (PARTITION BY home_team_id) AS goals1, 
-        SUM(away_team_goals) OVER (PARTITION BY home_team_id) AS goals2,
-        SUM(CASE
-            WHEN home_team_goals > away_team_goals THEN 3
-            WHEN home_team_goals < away_team_goals THEN 0
-            ELSE 1
-        END) OVER (PARTITION BY home_team_id) AS point1,
-        COUNT(*) OVER (PARTITION BY home_team_id) AS h_matches
-    FROM Matches
-),
-a AS (
-    SELECT 
-        DISTINCT away_team_id AS team_id, 
-        SUM(away_team_goals) OVER (PARTITION BY away_team_id) AS goals3, 
-        SUM(home_team_goals) OVER (PARTITION BY away_team_id) AS goals4, 
-        SUM(CASE
-            WHEN home_team_goals > away_team_goals THEN 0
-            WHEN home_team_goals < away_team_goals THEN 3
-            ELSE 1
-        END) OVER (PARTITION BY away_team_id) AS point2,
-        COUNT(*) OVER (PARTITION BY away_team_id) AS a_matches
-    FROM Matches
+WITH t AS (
+    (SELECT home_team_id AS team_id, CASE 
+        WHEN home_team_goals > away_team_goals THEN 3
+        WHEN home_team_goals < away_team_goals THEN 0
+        ELSE 1 END AS points, home_team_goals AS goal_for, away_team_goals AS goal_against
+    FROM Matches)
+    UNION ALL
+    (SELECT away_team_id AS team_id, CASE 
+        WHEN home_team_goals > away_team_goals THEN 0
+        WHEN home_team_goals < away_team_goals THEN 3
+        ELSE 1 END AS points, away_team_goals AS goal_for, home_team_goals AS goal_against
+    FROM Matches)
 )
-SELECT 
-    team_name, 
-    COALESCE(h_matches,0) + COALESCE(a_matches,0) AS matches_played,
-    COALESCE(point1,0) + COALESCE(point2,0) AS points,
-    COALESCE(goals1,0) + COALESCE(goals3,0) AS goal_for,
-    COALESCE(goals2,0) + COALESCE(goals4,0) AS goal_against,
-    (COALESCE(goals1,0) + COALESCE(goals3,0)) - (COALESCE(goals2,0) + COALESCE(goals4,0)) AS goal_diff
+SELECT team_name, COUNT(t.team_id) AS matches_played, COALESCE(SUM(points),0) AS points,
+    COALESCE(SUM(goal_for),0) AS goal_for,
+    COALESCE(SUM(goal_against),0) AS goal_against,
+    COALESCE(SUM(goal_for),0) - COALESCE(SUM(goal_against),0) AS goal_diff
 FROM Teams
-LEFT OUTER JOIN h ON Teams.team_id = h.team_id
-LEFT OUTER JOIN a ON Teams.team_id = a.team_id
-WHERE COALESCE(h_matches,0) + COALESCE(a_matches,0) != 0
+RIGHT OUTER JOIN t ON Teams.team_id = t.team_id
+GROUP BY Teams.team_id
 ORDER BY points DESC, goal_diff DESC, team_name;
