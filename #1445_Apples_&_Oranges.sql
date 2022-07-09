6e570d142a0075c1be997825db71376b9e566aa7
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 03:41:07 -0700

    Update #1445_Apples_&_Oranges.sql

--- #1445_Apples_&_Oranges.sql
+++ #1445_Apples_&_Oranges.sql
@@ -1,25 +1,3 @@
-WITH a_tab AS (
-    SELECT
-        sale_date, sold_num AS apples
-    FROM
-        Sales
-    WHERE
-        fruit = "apples"
-),
-o_tab AS (
-    SELECT
-        sale_date, sold_num AS oranges
-    FROM
-        Sales
-    WHERE
-        fruit = "oranges"
-),
-date_tab AS ( SELECT DISTINCT sale_date FROM Sales )
-SELECT
-    date_tab.sale_date, 
-    (COALESCE (a_tab.apples, 0) - COALESCE (o_tab.oranges, 0)) AS diff
-FROM
-    date_tab
-LEFT OUTER JOIN a_tab ON date_tab.sale_date = a_tab.sale_date
-LEFT OUTER JOIN o_tab ON date_tab.sale_date = o_tab.sale_date
-ORDER BY sale_date
+SELECT DISTINCT sale_date, SUM(IF(fruit = 'apples', sold_num, -sold_num)) OVER (PARTITION BY sale_date) AS diff
+FROM Sales
+ORDER By sale_date;
