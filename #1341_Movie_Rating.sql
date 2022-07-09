d8a14a9388229328ba06c9d687f0a2793b2b7198
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 01:54:44 -0700

    Update #1341_Movie_Rating.sql

--- #1341_Movie_Rating.sql
+++ #1341_Movie_Rating.sql
@@ -18,3 +18,18 @@ UNION
 (SELECT results FROM t2
 ORDER BY avgrate DESC, results
 LIMIT 1)
+-------
+WITH t1 AS (
+    SELECT DISTINCT user_id, COUNT(1) OVER (PARTITION BY user_id) AS cnt
+    FROM MovieRating
+),
+t2 AS (
+    SELECT DISTINCT movie_id, AVG(rating) OVER (PARTITION BY movie_id) AS score
+    FROM MovieRating
+    WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+)
+(SELECT name AS results FROM Users WHERE user_id IN (SELECT user_id FROM t1 WHERE cnt = (SELECT MAX(cnt) FROM t1))
+ORDER BY name LIMIT 1)
+UNION
+(SELECT title AS results FROM Movies WHERE movie_id IN (SELECT movie_id FROM t2 WHERE score = (SELECT MAX(score) FROM t2))
+ORDER BY title LIMIT 1)
