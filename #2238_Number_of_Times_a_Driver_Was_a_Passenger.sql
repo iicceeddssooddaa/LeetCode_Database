d236ee58dff956dfe07fd12bcee44feeee8e6470
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 17:41:53 -0700

    Update #2238_Number_of_Times_a_Driver_Was_a_Passenger.sql

--- #2238_Number_of_Times_a_Driver_Was_a_Passenger.sql
+++ #2238_Number_of_Times_a_Driver_Was_a_Passenger.sql
@@ -1,16 +1,3 @@
-WITH driver AS ( SELECT DISTINCT driver_id FROM Rides),
-passenger AS (
-    SELECT
-        passenger_id, COUNT(passenger_id) AS cnt
-    FROM
-        Rides
-    WHERE
-        (passenger_id IN (SELECT * FROM driver))
-    GROUP BY
-        passenger_id
-)
-SELECT
-    driver_id, COALESCE(cnt,0) AS cnt
-FROM
-    driver
-LEFT OUTER JOIN passenger ON driver_id = passenger_id
+SELECT DISTINCT r1.driver_id, COALESCE(cnt,0) AS cnt
+FROM Rides AS r1
+LEFT OUTER JOIN (SELECT passenger_id, COUNT(1) AS cnt FROM Rides GROUP BY passenger_id) AS r2 ON r1.driver_id = r2.passenger_id;
