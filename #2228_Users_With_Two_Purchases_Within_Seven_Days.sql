80c7bde502a2bc9caac2b14c0742c3fea14bd82a
iced soda <84180777+iicceeddssooddaa@users.noreply.github.com>
2022-07-09 17:31:09 -0700

    Update #2228_Users_With_Two_Purchases_Within_Seven_Days.sql

--- #2228_Users_With_Two_Purchases_Within_Seven_Days.sql
+++ #2228_Users_With_Two_Purchases_Within_Seven_Days.sql
@@ -1,10 +1,6 @@
-WITH t AS (
-    SELECT purchase_id, user_id,
-        DATEDIFF(purchase_date, lag(purchase_date) OVER (PARTITION BY user_id ORDER BY purchase_date)) AS diff
-    FROM Purchases
-    ORDER BY user_id, purchase_date
-)
 SELECT DISTINCT user_id
-FROM t
-WHERE diff <= 7
-ORDER BY user_id;
+FROM (
+    SELECT user_id, DATEDIFF(purchase_date, LAG(purchase_date) OVER (PARTITION BY user_id ORDER BY purchase_date)) <= 7 AS flag
+    FROM Purchases
+) AS t
+WHERE flag;
